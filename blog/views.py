14cff47ac7d965353acc98275d2f6adaab8f6a5f
barrysandy <865815412@qq.com>
2019-12-16 21:11:49 +0800

    1216

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+from .models import Tag, Post
+
+
+def post_list(request, category_id=None, tag_id=None):
+    # content = 'post_line category_id={category_id}, tag_id={tag_id}'.format(
+    #     category_id=category_id,
+    #     tag_id=tag_id,
+    # )
+    #
+    # return HttpResponse(content)
+
+    if tag_id:
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoseNotExist:
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    else:
+        post_list = Post.objects.filter(category_id=category_id)
+
+    return render(request, 'blog/list.html', context={'post_list': post_list})
+
+
+def post_detail(request, post_id):
+    # return HttpResponse('detail')
+    return render(request, 'blog/detail.html', context={'name': 'post_detail'})
--- typeidea/urls.py
+++ typeidea/urls.py
@@ -4,10 +4,18 @@ from django.conf.urls import url
 
 from typeideas.views import IndexView
 
+from blog.views import post_list, post_detail
+from config.views import links
 from config.custom_site import custom_site
 
 urlpatterns = [
+    url(r'^$', post_list),
+    url(r'^category/(?P<category_id>\d+)/$', post_list),
+    url(r'^tag/(?P<tag_id>\d+)/$', post_list),
+    url(r'^post/(?P<post_id>\d+).html$', post_detail),
+    url(r'^links/$', links),
+
+    url(r'^$', IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
     path('admin_blog/', custom_site.urls),
-    url(r'^$', IndexView.as_view(), name='index')
 ]
